0377993d1b63c5473db170d9515e49a6dc3fe00a
Luchs <Luchs@euirc.eu>
2014-05-08 21:17:43 +0200

    Make it work properly
     - The script is already executing in <head>.
     - Also format the preview panes on post_add and post_edit.
     - Better blockquote handling.

--- src/marked.js
+++ src/marked.js
@@ -1,14 +1,17 @@
 // Format all posts as markdown.
-$('.pst > .ccl:nth-child(2)').each(function() {
-  var el, text;
+$(function() {
+  $('.pst > .ccl:nth-child(2), body[class^=post] > .frm > .ccl').each(function() {
+	var el, text;
 
-  el = $(this);
-  text = el.html();
+	el = $(this);
+	text = el.html();
 
-  // Replace mwforum's <br>s with newlines so that paragraph detection works.
-  text = text.replace(/<br>/g, '\n');
-  // Remove blockquotes as they confuse marked.
-  text = text.replace(/<\/?blockquote>/g, '');
+	// Replace mwforum's <br>s with newlines so that paragraph detection works.
+	text = text.replace(/<br>/g, '\n');
+	// Remove blockquotes as they confuse marked.
+	text = text.replace(/<blockquote><p>/g, '>');
+	text = text.replace(/<\/p><\/blockquote>/g, '\n');
 
-  el.html(marked(text));
+	el.html(marked(text));
+  });
 });
