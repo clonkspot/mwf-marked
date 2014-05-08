f4174aca3341941b091b351042548483554fa802
Luchs <Luchs@euirc.eu>
2014-05-08 20:54:18 +0200

    Add mwf plugin file

---
+++ script/MwfPlgMarked.pm
@@ -0,0 +1,12 @@
+package MwfPlgMarked;
+use strict;
+use warnings;
+
+sub htmlHeader {
+	my %params = @_;
+	my $m = $params{m};
+	return unless $m->{env}{script} =~ /_show|forum_overview/;
+	print "<script type='text/javascript' src='$m->{cfg}{dataPath}/mwf-marked.js'></script>\n";
+}
+
+1;
