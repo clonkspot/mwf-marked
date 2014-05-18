2588b1baf89fb4964cb3074f2bbd22a1b6c49ecf
Luchs <Luchs@euirc.eu>
2014-05-18 16:26:21 +0200

    Load unminified version for admins
    This allows easier testing in production...

--- script/MwfPlgMarked.pm
+++ script/MwfPlgMarked.pm
@@ -6,7 +6,8 @@ sub htmlHeader {
 	my %params = @_;
 	my $m = $params{m};
 	return unless $m->{env}{script} =~ /_show|forum_overview|_edit|_add/;
-	print "<script type='text/javascript' src='$m->{cfg}{dataPath}/mwf-marked.js'></script>\n";
+	my $min = $m->{user}{admin} ? '' : '.min';
+	print "<script type='text/javascript' src='$m->{cfg}{dataPath}/mwf-marked$min.js'></script>\n";
 }
 
 1;
